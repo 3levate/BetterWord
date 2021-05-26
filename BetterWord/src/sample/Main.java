d3e7b79066759ed84598afbebf8d4915c78b5237
3levate <58492326+3levate@users.noreply.github.com>
2021-05-26 15:02:18 -0700

    Add files via upload

--- BetterWord/src/sample/Main.java
+++ BetterWord/src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -12,8 +14,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        BorderPane root = new BorderPane();
 
-        primaryStage.setTitle("Hello World");
+
+        primaryStage.setTitle("BetterWord");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
