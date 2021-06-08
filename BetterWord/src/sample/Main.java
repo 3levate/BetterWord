acf6ac4826984141827d74ecbd33630efc9a4b70
3levate <58492326+3levate@users.noreply.github.com>
2021-06-08 15:03:03 -0700

    Add files via upload

--- BetterWord/src/sample/Main.java
+++ BetterWord/src/sample/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
+        root.getStylesheets().add("package/css1.css");
 
 
         primaryStage.setTitle("BetterWord");
