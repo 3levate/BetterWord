a35c762db6de43623e9689c7a627faf9747ab5af
3levate <58492326+3levate@users.noreply.github.com>
2021-06-09 15:02:31 -0700

    Add files via upload

--- BetterWord/src/sample/Controller.java
+++ BetterWord/src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    @FXML
+    private TextArea inputArea;
+
+    @FXML
+    private Text sixsixsix;
+
+    public void initialize() {
+        System.out.println(inputArea);
+        if (inputArea.charAt() == sixsixsix.getText()) {
+            sixsixsix.setStroke(Color.BLUE);
+            System.out.println(inputArea);
+        }
+        System.out.println(inputArea);
+    }
 }
--- BetterWord/src/sample/Main.java
+++ BetterWord/src/sample/Main.java
@@ -1,22 +1,33 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.util.Scanner;
+
+
 public class Main extends Application {
+
+    private TextArea inputArea;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        root.getStylesheets().add("package/css1.css");
+//        root.getStylesheets().add("package/css1.css");
 
+
+        System.out.println(inputArea);
 
         primaryStage.setTitle("BetterWord");
         primaryStage.setScene(new Scene(root, 300, 275));
