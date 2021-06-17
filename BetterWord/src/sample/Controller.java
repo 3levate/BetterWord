611b204cc5ef458742fb1196845b3c79ef6a5c29
3levate <58492326+3levate@users.noreply.github.com>
2021-06-17 15:04:30 -0700

    Add files via upload

--- BetterWord/src/sample/Controller.java
+++ BetterWord/src/sample/Controller.java
@@ -1,11 +1,14 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -25,6 +28,9 @@ public class Controller extends Main implements Initializable {
     private Text sixsixsix;
 
     @FXML
+    private StackPane typingArea;
+
+    @FXML
     private void btnClick(ActionEvent event) {
         System.out.println(input.getText());
 
@@ -37,13 +43,31 @@ public class Controller extends Main implements Initializable {
         }
     }
 
+    @FXML
+    private void detectInputAreaKeyEvent(KeyEvent event) {
+        char[] splitter = textToType.toCharArray();
+        for (int i = splitter.length; i < )
+        if (event.getText().equals()) {
+            System.out.println("working");
+
+        }
+        else {
+            System.out.println("wrong key");
+        }
+    }
+
+    @FXML
+    private void detectLetterTyped(KeyEvent event) {
+
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        input.getScene().setOnKeyPressed((KeyEvent event) -> {
-            if (input.getScene().getOnKeyTyped().equals("B")) {
-
-            }
-        });
+//        input.getScene().setOnKeyPressed((KeyEvent event) -> {
+//
+//        });
+        sixsixsix.setText(textToType);
     }
 }
--- BetterWord/src/sample/Main.java
+++ BetterWord/src/sample/Main.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.application.Application;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +11,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,8 +24,17 @@ import java.util.Scanner;
 
 public class Main extends Application {
 
+    @FXML
+    private StackPane typingArea;
+
+    @FXML
+    private Text sixsixsix;
+
+    String textToType = "urmom";
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 //        root.getStylesheets().add("package/css1.css");
 
@@ -30,6 +43,27 @@ public class Main extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
+//        typingArea.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+//            @Override
+//            public void handle(KeyEvent keyEvent) {
+//
+//                System.out.println("working from controller");
+//            }
+//        });
+//        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+//            @Override
+//            public void handle(KeyEvent keyEvent) {
+//                if (keyEvent.getText().equals(currentLetter )) {
+//                    System.out.println("right letter pressed");
+//                    sixsixsix.get
+//                }
+//                else {
+//                    System.out.println("other letter was pressed");
+//                }
+//            }
+//        });
+
 
     }
 
