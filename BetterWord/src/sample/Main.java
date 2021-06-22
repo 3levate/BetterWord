2c335448dabdded28c0912c5ea95e8eb3e458560
3levate <jacecraft29@gmail.com>
2021-06-21 23:39:58 -0700

    pogers

--- BetterWord/src/sample/Main.java
+++ BetterWord/src/sample/Main.java
@@ -9,11 +9,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     char nextLine = '\n';
-    String textToType = "as " + "his " + "that " + "he " + "was " + "for " + "on " + "are " + "with " + "they " + "be " + "at " + "one " + "have " + "this " + "from " + "by " + "hot " + "word " + "but" + nextLine +
+    String textToType =
+            "as " + "his " + "that " + "he " + "was " + "for " + "on " + "are " + "with " + "they " + "be " + "at " + "one " + "have " + "this " + "from " + "by " + "hot " + "word " + "but " + nextLine +
             "what " + "some " + "is " + "it " + "you " + "or " + "had " + "the " + "of " + "to " + "and " + "a " + "in " + "we " + "can " + "out " + "other " + "were " + "which " + "do " + "their " + nextLine +
-            "time " + "if" + "will " + "how " + "said " + "an " + "each " + "tell " + "does " + "set " + "three " + "want " + "air " + "well " + "also " + "play " + "small " +"end " + "put " + "home " +nextLine
-            + "read " + "hand " + "port " + "large " + "spell " + "add " + "even" + "land " + "here " + "must " + "big " + "high " + "such " + "follow " + "act " + "why " + "ask " + "men " + "change " + nextLine
-            + "went " + "light " + "kind " + "off " + "need " + "house " + "picture " + "try " + "us " + "again " + "animal " + "point " + "mother " + "world " + "near " + "build " + "self " + "earth " + "father";
+            "time " + "if " + "will " + "how " + "said " + "an " + "each " + "tell " + "does " + "set " + "three " + "want " + "air " + "well " + "also " + "play " + "small " +"end " + "put " + nextLine +
+            "home " + "read " + "hand " + "port " + "large " + "spell " + "add " + "even" + "land " + "here " + "must " + "big " + "high " + "such " + "follow " + "act " + "why " + "ask " + nextLine +
+            "men " + "change " + "went " + "light " + "kind " + "off " + "need " + "house " + "picture " + "try " + "us " + "again " + "animal " + "point " + "mother " + "world " + nextLine +
+            "near " + "build " + "self " + "earth " + "father";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
